b969180b87d7c89ad570d8fb2ecc4a878ef79f07
Steve Thomas <steve13thomas@gmail.com>
2024-06-09 14:53:53 +0100

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    
+    <div>
+      <div className='text-red-500'>App</div>
+    </div>
+  )
+}
+
+export default App
