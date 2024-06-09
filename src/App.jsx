d82031c4111c2977c82c453a1b4cd0d91ebae0f5
Steve Thomas <steve13thomas@gmail.com>
2024-06-09 15:05:17 +0100

    updated with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
     
-    <div>
-      <div className='text-red-500'>App</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={< Home />}></Route>
+        <Route path="/about" element={< About />}></Route>
+        <Route path="/profile" element={< Profile />}></Route>
+        <Route path="/sign-in" element={< SignIn />}></Route>
+        <Route path="/sign-up" element={< SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
