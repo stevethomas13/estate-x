c0276ad332b60608fb70b53083afcacaf532aece
Steve Thomas <steve13thomas@gmail.com>
2024-06-14 22:30:20 +0100

    compeleted sign in page

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { signup, signin } from '../controllers/auth.controller.js';
-import { all } from '../controllers/auth.controller.js';
+import { signup, signin, all } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup',signup)
 router.post('/signin',signin)
-router.post('/all',all)
+router.get('/all',all)
 
 export default router;
