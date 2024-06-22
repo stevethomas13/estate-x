184bf24f63d67914a9abbf00c1a24597eede864a
Steve Thomas <steve13thomas@gmail.com>
2024-06-22 13:39:57 +0100

    fixed google oauth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,14 +38,13 @@ export const signin = async (request, response, next) => {
 
 }
 
-export const google = async (request, response, next) => {
-    
+export const google = async (req, res, next) => {
     try {
-        const user = await User.findOne({email: request.body.email});
+        const user = await User.findOne({ email: req.body.email });
         if (user){
-            const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-            const {password: pass, ...rest} = validUser._doc;
-            response
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rest } = user._doc;
+            res
                 .cookie('access_token', token, {httpOnly: true})
                 .status(200)
                 .json(rest)
@@ -53,13 +52,13 @@ export const google = async (request, response, next) => {
         else{
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-            const generatedUserName = request.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4);
-            const newUser = new User({username: generatedUserName, email: request.body.email, 
-                password: hashedPassword, avatar: request.body.photo});
+            const generatedUserName = req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4);
+            const newUser = new User({username: generatedUserName, email: req.body.email, 
+                password: hashedPassword, avatar: req.body.photo});
             await newUser.save();
             const token = jwt.sign({id: newUser._id} , process.env.JWT_SECRET);
             const {password: pass, ...rest} = newUser._doc;
-            response.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+            res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
         }
         
             
