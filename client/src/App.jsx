7a6581c70a5e1ea22199ecbb95a5d28e3bca7ea3
Steve Thomas <steve13thomas@gmail.com>
2024-06-27 12:09:56 +0100

    add image slider to listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/about" element={< About />}></Route>
         <Route path="/sign-in" element={< SignIn />}></Route>
         <Route path="/sign-up" element={< SignUp />}></Route>
+        <Route path="/listing/:listingId" element={< Listing />}></Route>
         <Route element={< PrivateRoute />}>
           <Route path="/create-listing" element={< CreateListing />}></Route>
           <Route path="/update-listing/:listingId" element={< UpdateListing />}></Route>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -201,11 +201,11 @@ const Profile = () => {
         <h1 className='text-center mt-7 text-2xl font-semibold'>Your listings</h1>
         {userListings.map((listing) => (
           <div key={listing._id} className='border hover:shadow-lg rounded-lg p-3 flex justify-between items-center'>
-              <Link to={`/listings/${listing._id}`}>
+              <Link to={`/listing/${listing._id}`}>
                 <img src={listing.imageUrls[0]} alt="listing cover" 
                 className='h-16 w-16 object-contain' />
               </Link>
-              <Link to={`/listings/${listing._id}`}>
+              <Link to={`/listing/${listing._id}`}>
                 <p className='text-slate-700 font-semibold hover:underline truncate flex-1'>{listing.name}</p>
               </Link>
               <div className='flex flex-col items-center'>
