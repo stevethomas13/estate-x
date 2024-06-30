31fc058751d9053fdd784f83baad75a28e4928d9
Steve Thomas <steve13thomas@gmail.com>
2024-06-30 19:56:15 +0100

    added search form to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const Navbar = () => {
 
     const {currentUser} = useSelector(state => state.user) 
+    const [searchTerm, setSearchTerm ] = useState('');
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    }
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
 
     return (
         <header className='bg-slate-200 shadown-md'>
@@ -17,12 +35,19 @@ const Navbar = () => {
                     </h1>
                 </Link>
 
-                <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <form 
+                    className='bg-slate-100 p-3 rounded-lg flex items-center'
+                    onSubmit={handleSubmit}>
                     <input
                         type="text"
                         placeholder='Search..'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                         className='bg-transparent focus:outline-none w-24 sm:w-64'
                     />
+                    <button>
+                        <FaSearch className='text-slate-600'></FaSearch>
+                    </button>
                 </form>
                 <ul className='flex gap-4'>
                     <Link to='/'>
