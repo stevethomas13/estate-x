c8f2c7dad2f261d1e67b7d7f42a0cc295223654b
Steve Thomas <steve13thomas@gmail.com>
2024-07-01 14:13:14 +0100

    fixed button spelling

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -63,7 +63,7 @@ const Listing = () => {
           <Swiper navigation>
             {listing.imageUrls.map((url) =>
               ( <SwiperSlide key={url}>
-                  <img className='h-[500px] mx-auto' src={url}></img>
+                  <img className='h-[500px] mx-auto object-cover w-full' src={url}></img>
                 </SwiperSlide>
               )
             )}
@@ -103,7 +103,7 @@ const Listing = () => {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice}
+                  ${+listing.regularPrice - +listing.discountPrice} DISCOUNT
                 </p>
               )}
             </div>
