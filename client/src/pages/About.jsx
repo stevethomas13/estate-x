26ab28dbfe88651dcfa4e61d7046516084227688
Steve Thomas <steve13thomas@gmail.com>
2024-07-01 14:13:31 +0100

    created about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Estate X</h1>
+      <p className='mb-4 text-slate-700'>
+      Welcome to Estate X, your premier destination for buying and selling homes. Whether you're in the market for your dream house or looking to get the best value for your property, Estate X offers a seamless and efficient platform tailored to meet your real estate needs. Our extensive listings and personalized services ensure you find exactly what you're looking for, with trusted buyers and sellers ready to make your transaction smooth and successful.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      At Estate X, we understand that buying or selling a home is more than just a transaction—it's a significant life event. That's why our team is dedicated to providing you with comprehensive support and expert guidance every step of the way. From detailed property listings and virtual tours to market analysis and negotiation tips, we equip you with the tools and knowledge needed to make informed decisions with confidence.
+      </p>
+      <p className='mb-4 text-slate-700'>
+      Join our vibrant community of homeowners, buyers, and real estate professionals who trust Estate X to deliver exceptional results. Our user-friendly platform makes it easy to list your property, explore potential homes, and connect with the right people. Start your real estate journey with Estate X today, and let us help you turn your dreams into reality.
+      </p>
+    </div>
   )
 }
 
