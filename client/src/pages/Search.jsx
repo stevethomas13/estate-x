03e0f21d13bdcf37830cc2e7740d33887d4822f1
Steve Thomas <steve13thomas@gmail.com>
2024-07-01 14:12:50 +0100

    create listing item to show on search

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 
 const Search = () => {
@@ -17,7 +18,6 @@ const Search = () => {
     });
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-    console.log(listings);
 
     useEffect(() => {
       
@@ -196,13 +196,23 @@ const Search = () => {
                     Search
                 </button>
             </form>
-            <div>
+        </div>
+            <div className='flex-1'>
                 <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
                     Listing results
                 </h1>
+                <div className='p-7 flex flex-wrap gap-4'>
+                    {!loading && listings.length === 0 && (
+                        <p className='text-xl text-slate-700'>No listing found</p>
+                    )}
+                    {loading && (
+                        <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                    )}
+                    {!loading && listings && listings.map((listing) => (
+                        <ListingItem key={listing._id} listing={listing}></ListingItem>
+                    ))}
+                </div>
             </div>
-
-        </div>
     </div>
   )
 }
