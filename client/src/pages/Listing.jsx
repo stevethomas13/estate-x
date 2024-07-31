3c2b380856378a85036e682007a868e4c55aaf02
Steve Thomas <steve13thomas@gmail.com>
2024-07-31 11:20:56 +0100

    improved ui

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -17,10 +17,13 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 const Listing = () => {
   SwiperCore.use([Navigation]);
   const {currentUser} = useSelector(state => state.user);
+  const [offerListings, setOfferListings] =useState();
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -42,11 +45,22 @@ const Listing = () => {
           return ;
         }
         setListing(data);
+        fetchOfferListings();
         setLoading(false);
       } catch (error) {
         setError(true);
         setLoading(false);
       }  
+    }
+
+    const fetchOfferListings = async () => {
+        try{
+            const res = await fetch('/api/listing/get?offer=true&limit=4');
+            const data = await res.json();
+            setOfferListings(data);
+        }catch(error){
+            console.log(error);
+        }
     }
 
     fetchListing();    
@@ -145,6 +159,27 @@ const Listing = () => {
 
         </div>
       )}
+
+      <div className="max-w-7xl mx-auto p-3 flex flex-col gap-8 my-10">
+          
+           { offerListings && offerListings.length > 0 && (
+              <div>
+                  <div className="my-3">
+                    <h2 className="text-2xl font-semibold text-slate-600">More like this:</h2>
+                    <Link className="text-sm text-blue-800 hover:underline" to={'../search?offer=true'}>
+                        Show more offers
+                    </Link>
+                  </div>
+                  <div className="flex flex-wrap gap-4">
+                      {offerListings.map((offerListing) => (
+                          <ListingItem listing={offerListing} key={offerListing._id}></ListingItem>
+                      ))}
+                  </div>
+              </div>
+          )} 
+          
+      </div>
+
       <Footer></Footer>
     </main>
   )
